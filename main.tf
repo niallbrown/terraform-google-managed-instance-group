a170af9873beb4bee3105d61e613a23cb8ebbea0
Niall Brown <niall.brown@lightspeedhq.com>
2019-05-21 16:50:27 +0200

    fix
    fix missed health

--- main.tf
+++ main.tf
@@ -162,10 +162,6 @@ resource "google_compute_region_instance_group_manager" "default" {
   // Issue: https://github.com/terraform-providers/terraform-provider-google/issues/667
   target_size = "${var.autoscaling ? var.min_replicas : var.size}"
 
-  auto_healing_policies {
-    health_check      = "${var.http_health_check ? element(concat(google_compute_health_check.mig-health-check.*.self_link, list("")), 0) : ""}"
-    initial_delay_sec = "${var.hc_initial_delay}"
-  }
 
   named_port {
     name = "${var.service_port_name}"
@@ -182,10 +178,6 @@ resource "google_compute_region_instance_group_manager" "default" {
     command = "${var.local_cmd_create}"
   }
 
-  // Initial instance verification can take 10-15m when a health check is present.
-  timeouts = {
-    create = "${var.http_health_check ? "15m" : "5m"}"
-  }
 }
 
 resource "google_compute_region_autoscaler" "default" {
@@ -238,36 +230,6 @@ resource "google_compute_firewall" "default-ssh" {
   target_tags   = ["allow-ssh"]
 }
 
-resource "google_compute_health_check" "mig-health-check" {
-  count   = "${var.module_enabled && var.http_health_check ? 1 : 0}"
-  name    = "${var.name}"
-  project = "${var.project}"
-
-  check_interval_sec  = "${var.hc_interval}"
-  timeout_sec         = "${var.hc_timeout}"
-  healthy_threshold   = "${var.hc_healthy_threshold}"
-  unhealthy_threshold = "${var.hc_unhealthy_threshold}"
-
-  http_health_check {
-    port         = "${var.hc_port == "" ? var.service_port : var.hc_port}"
-    request_path = "${var.hc_path}"
-  }
-}
-
-resource "google_compute_firewall" "mig-health-check" {
-  count   = "${var.module_enabled && var.http_health_check ? 1 : 0}"
-  project = "${var.subnetwork_project == "" ? var.project : var.subnetwork_project}"
-  name    = "${var.name}-vm-hc"
-  network = "${var.network}"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["${var.hc_port == "" ? var.service_port : var.hc_port}"]
-  }
-
-  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
-  target_tags   = ["${var.target_tags}"]
-}
 
 data "google_compute_instance_group" "zonal" {
   count   = "${var.zonal ? 1 : 0}"
